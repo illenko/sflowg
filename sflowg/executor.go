98182a5427583918f6e2f14b17a2769f077b706a
illenko <kostiaillenko@gmail.com>
2025-03-15 18:39:02 +0200

    Refactor executor

--- sflowg/executor.go
+++ sflowg/executor.go
@@ -7,7 +7,6 @@ import (
 )
 
 func ExecuteSteps(e *Execution) error {
-
 	nextStep := ""
 
 	for _, s := range e.Flow.Steps {
@@ -15,102 +14,133 @@ func ExecuteSteps(e *Execution) error {
 			if s.ID != nextStep {
 				fmt.Printf("Skipping step: %s, searching for %s\n", s.ID, nextStep)
 				continue
-			} else {
-				nextStep = ""
-				fmt.Printf("Found expected step: %s\n", s.ID)
 			}
+			nextStep = ""
+			fmt.Printf("Found expected step: %s\n", s.ID)
 		}
 
-		if s.Condition != "" {
-			result, err := Eval(s.Condition, e.Values)
-			if err != nil {
-				fmt.Printf("Error evaluating expression: %v\n", err)
-				return err
-			}
-
-			if !result.(bool) {
-				fmt.Printf("Skipping step: %s\n", s.ID)
-				continue
-			} else {
-				fmt.Printf("Condition %s is satisfied for step: %s\n", s.Condition, s.ID)
-			}
-
+		if err := evaluateCondition(e, s); err != nil {
+			fmt.Printf("Skipping step: %s\n", s.ID)
+			continue
 		}
 
-		if s.Type == "assign" {
-			for k, v := range s.Args {
-				result, err := Eval(v.(string), e.Values)
-				if err != nil {
-					fmt.Printf("Error evaluating expression: %v\n", err)
-					return err
-				}
-				e.AddVal(k, result)
-			}
-		} else if s.Type == "switch" {
-			for n, c := range s.Args {
-				condition := c.(string)
+		if err := executeStepType(e, s, &nextStep); err != nil {
+			return err
+		}
 
-				result, err := Eval(condition, e.Values)
-				if err != nil {
-					fmt.Printf("Error evaluating expression: %v\n", err)
-					return err
-				}
-
-				resultBool, ok := result.(bool)
-
-				if !ok {
-					fmt.Printf("Error evaluating expression: %v\n", err)
-					return fmt.Errorf("condition %s is not a boolean", condition)
-				}
-
-				if resultBool {
-					fmt.Printf("Resolving switch: %s is true, next step is %s\n", condition, n)
-					nextStep = n
-					break
-				} else {
-					fmt.Printf("Resolving switch: %s is false\n", condition)
-				}
-			}
-		} else {
-			task, ok := e.Container.Tasks[s.Type]
-
-			if !ok {
-				fmt.Printf("Task type: %s not found\n", s.Type)
-				return fmt.Errorf("Task type: %s not found", s.Type)
-			}
-
-			executeTask(e, task, s)
-
-			fmt.Printf("Step %s executed\n", s.ID)
-
-			if s.Retry != nil {
-				for i := 0; i < s.Retry.MaxRetries; i++ {
-					condition, err := Eval(s.Retry.Condition, e.Values)
-
-					fmt.Printf("[%s/%s] Retrying step: %s, condition: %v\n", strconv.Itoa(i+1), strconv.Itoa(s.Retry.MaxRetries), s.ID, condition)
-
-					if err != nil {
-						fmt.Printf("Error evaluating expression: %v\n", err)
-						return err
-					}
-
-					if !condition.(bool) {
-						break
-					}
-
-					if s.Retry.Backoff {
-						time.Sleep(time.Duration(i*s.Retry.Delay) * time.Millisecond)
-					} else {
-						time.Sleep(time.Duration(s.Retry.Delay) * time.Millisecond)
-					}
-
-					executeTask(e, task, s)
-
-				}
-			}
+		if err := handleRetry(e, s); err != nil {
+			return err
 		}
 	}
 
+	return nil
+}
+
+func evaluateCondition(e *Execution, s Step) error {
+	if s.Condition == "" {
+		return nil
+	}
+
+	result, err := Eval(s.Condition, e.Values)
+	if err != nil {
+		fmt.Printf("Error evaluating expression: %v\n", err)
+		return err
+	}
+
+	if !result.(bool) {
+		fmt.Printf("Skipping step: %s\n", s.ID)
+		return fmt.Errorf("condition not met") // return an error so that the calling function knows the step was skipped, but continue.
+	}
+	fmt.Printf("Condition %s is satisfied for step: %s\n", s.Condition, s.ID)
+	return nil
+}
+
+func executeStepType(e *Execution, s Step, nextStep *string) error {
+	switch s.Type {
+	case "assign":
+		return handleAssign(e, s)
+	case "switch":
+		return handleSwitch(e, s, nextStep)
+	default:
+		return handleTask(e, s)
+	}
+}
+
+func handleAssign(e *Execution, s Step) error {
+	for k, v := range s.Args {
+		result, err := Eval(v.(string), e.Values)
+		if err != nil {
+			fmt.Printf("Error evaluating expression: %v\n", err)
+			return err
+		}
+		e.AddVal(k, result)
+	}
+	return nil
+}
+
+func handleSwitch(e *Execution, s Step, nextStep *string) error {
+	for n, c := range s.Args {
+		condition := c.(string)
+		result, err := Eval(condition, e.Values)
+		if err != nil {
+			fmt.Printf("Error evaluating expression: %v\n", err)
+			return err
+		}
+		resultBool, ok := result.(bool)
+		if !ok {
+			return fmt.Errorf("condition %s is not a boolean", condition)
+		}
+		if resultBool {
+			fmt.Printf("Resolving switch: %s is true, next step is %s\n", condition, n)
+			*nextStep = n
+			return nil
+		}
+		fmt.Printf("Resolving switch: %s is false\n", condition)
+	}
+	return nil
+}
+
+func handleTask(e *Execution, s Step) error {
+	task, ok := e.Container.Tasks[s.Type]
+	if !ok {
+		fmt.Printf("Task type: %s not found\n", s.Type)
+		return fmt.Errorf("task type: %s not found", s.Type)
+	}
+	executeTask(e, task, s)
+	fmt.Printf("Step %s executed\n", s.ID)
+	return nil
+}
+
+func handleRetry(e *Execution, s Step) error {
+	if s.Retry == nil {
+		return nil
+	}
+
+	task, ok := e.Container.Tasks[s.Type]
+	if !ok {
+		return fmt.Errorf("Task type: %s not found", s.Type)
+	}
+
+	for i := 0; i < s.Retry.MaxRetries; i++ {
+		condition, err := Eval(s.Retry.Condition, e.Values)
+		fmt.Printf("[%s/%s] Retrying step: %s, condition: %v\n", strconv.Itoa(i+1), strconv.Itoa(s.Retry.MaxRetries), s.ID, condition)
+
+		if err != nil {
+			fmt.Printf("Error evaluating expression: %v\n", err)
+			return err
+		}
+
+		if !condition.(bool) {
+			break
+		}
+
+		delay := time.Duration(s.Retry.Delay) * time.Millisecond
+		if s.Retry.Backoff {
+			delay = time.Duration(i*s.Retry.Delay) * time.Millisecond
+		}
+		time.Sleep(delay)
+		executeTask(e, task, s)
+	}
 	return nil
 }
 
